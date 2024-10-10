3e8c0d9b8d5d3133d9fc305fb97d7834522a3c05
YesitongW <wuy2@nycstudents.net>
2024-10-10 12:44:30 +0000

    w2

--- lab3/Runner.java
+++ lab3/Runner.java
@@ -3,22 +3,29 @@ package lab3;
 public class Runner {
     public static void main(String[] args) {
         CustomStringMethods methods = new CustomStringMethods();
-        String str = methods.halvesReversed("computer");  //even # of letters
-        System.out.println(str);
-        str = methods.halvesReversed("computers");        //odd # of letters
-        System.out.println(str);
-        str = methods.halvesReversed("sky");
-        System.out.println(str);
-        str = methods.halvesReversed("weekend");
-        System.out.println(str);
-        str = methods.halvesReversed("an");
-        System.out.println(str);
-        str = methods.halvesReversed("I");
-        System.out.println(str);
-        str = methods.halvesReversed("see you soon");
-        System.out.println(str);
-        str = methods.halvesReversed("see you later!");
-        System.out.println(str);
+        boolean bool = methods.longerThan("bananas", 6);
+        System.out.println(bool);
+
+        String str1 = methods.funnyString("Math", 0);
+        System.out.println(str1);
+        
+        String str2 = methods.halvesReversed("monster");
+        System.out.println(str2);
+
+        String str3 = methods.pigLatin("avocados");
+        System.out.println(str3);
+        
+        String str4 = methods.removeCharacter("Google", 3);
+        System.out.println(str4);
+
+        String str5 = methods.insertAt("Jason", "Ahh!", "a");
+        System.out.println(str5);
+
+        String str6 = methods.endUp("i don't like history class", 13);
+        System.out.println(str6);
+        
+        String str7 = methods.yellOrWhisper("How are you?");
+        System.out.println(str7);
         
     }
 }
